0194984bb5620f0c6562f44075dc0c53b482ea12
pippi-chen <melody51327@gmail.com>
2021-11-12 21:37:33 -0500

    update

--- Easy/1484.Table Schema.sql
+++ Easy/1484.Table Schema.sql
@@ -1,3 +1,9 @@
+-- 1407. Top Travellers
+-- Write an SQL query to report the distance travelled by each user.
+-- Return the result table ordered by travelled_distance in descending order, 
+-- if two or more users travelled the same distance, order them by their name in ascending order.
+
+-- Create table
 DROP TABLE ACTIVITIES;
 CREATE TABLE ACTIVITIES(
  SELL_DATE DATE,
@@ -12,3 +18,8 @@ INSERT INTO ACTIVITIES VALUES ('2020-06-01', 'Bible');
 INSERT INTO ACTIVITIES VALUES ('2020-06-02', 'Mask');
 INSERT INTO ACTIVITIES VALUES ('2020-05-30', 'T-Shirt');
 COMMIT;
+
+-- Solution
+
+
+
